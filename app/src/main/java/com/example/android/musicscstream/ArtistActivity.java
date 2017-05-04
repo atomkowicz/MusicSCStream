040a9fbfaf9a3b73049204dc846593b4b01dc0cc
Agnieszka Tomkowicz <agntomk@gmail.com>
2017-05-04 14:09:29 +0200

    Layout, navigation

--- app/src/main/java/com/example/android/musicscstream/ArtistActivity.java
+++ app/src/main/java/com/example/android/musicscstream/ArtistActivity.java
@@ -6,7 +6,11 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ArtistActivity extends AppCompatActivity {
 
@@ -39,6 +43,41 @@ public class ArtistActivity extends AppCompatActivity {
         TextView artistAllSongsTextView = (TextView) findViewById(R.id.artist_songs_all);
         artistAllSongsTextView.setText(artistAllSongs + " songs");
 
+        LinearLayout album1 = (LinearLayout) findViewById(R.id.artist_album_1);
+        LinearLayout album2 = (LinearLayout) findViewById(R.id.artist_album_2);
+        LinearLayout album3 = (LinearLayout) findViewById(R.id.artist_album_3);
+
+        final ArrayList<Album> albums = artist.getAlbums();
+
+        album1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ArtistActivity.this, AlbumActivity.class);
+                intent.putExtra("album", albums.get(1));
+                startActivity(intent);
+
+            }
+        });
+
+        album2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ArtistActivity.this, AlbumActivity.class);
+                intent.putExtra("album", albums.get(2));
+                startActivity(intent);
+
+            }
+        });
+
+        album3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ArtistActivity.this, AlbumActivity.class);
+                intent.putExtra("album", albums.get(3));
+                startActivity(intent);
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/android/musicscstream/MainActivity.java
+++ app/src/main/java/com/example/android/musicscstream/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -220,26 +219,26 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                    Intent formIntend = new Intent();
+                    Intent intent = new Intent();
 
                     // Create new intent to open {@link TabbedActivity}
                     switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
                         case 1:
-                            formIntend = new Intent(getContext(), ArtistActivity.class);
-                            formIntend.putExtra("artist", artists.get(position));
+                            intent.setClass(getContext(), ArtistActivity.class);
+                            intent.putExtra("artist", artists.get(position));
                             break;
                         case 2:
-                            formIntend = new Intent(getContext(), AlbumActivity.class);
-                            formIntend.putExtra("album", albums.get(position));
+                            intent.setClass(getContext(), AlbumActivity.class);
+                            intent.putExtra("album", albums.get(position));
                             break;
                         case 3:
-                            formIntend = new Intent(getContext(), SongActivity.class);
-                            formIntend.putExtra("song", songs.get(position));
+                            intent.setClass(getContext(), SongActivity.class);
+                            intent.putExtra("song", songs.get(position));
                             break;
                     }
 
                     // Start the new activity
-                    startActivity(formIntend);
+                    startActivity(intent);
                 }
             });
 
